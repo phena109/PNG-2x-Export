83e21494fb17f2f63f6826b4103ed119dca96eac
douglas <douglas@192.168.0.18>
2014-04-20 15:26:22 +1000

    Use save dialog for filename and save path. Correct image resize issue.

--- PNG_2x_Export.jsx
+++ PNG_2x_Export.jsx
@@ -1,63 +1,71 @@
 #target photoshop
 
-app.bringToFront();  
+app.bringToFront();
 
-function main(){
+function main() {
 
-	var doc = app.activeDocument;
-	var psdPath = app.activeDocument.path;
-	var outFolderName="pngExport";
+    var doc = app.activeDocument;
 
-	var outFolder = new Folder(psdPath + "/"+outFolderName);
-	if (!outFolder.exists) {
-	    outFolder.create();
-	}
+    var lname = File.saveDialog('Please type in the file name WITHOUT the extension');
+    saveLayer(doc.activeLayer, lname, false);
 
-	var lname = doc.activeLayer.name;
-	saveLayer(doc.activeLayer, lname, psdPath, false);	
+    function saveLayer(layer, lname, shouldMerge) {
+        
+        var startRulerUnits = app.preferences.rulerUnits
+        var startTypeUnits = app.preferences.typeUnits
+        var startDisplayDialogs = app.displayDialogs
 
-	function saveLayer(layer, lname, path, shouldMerge) {
-	    dupLayers();
-	    if (shouldMerge === undefined || shouldMerge === true) {
-		activeDocument.mergeVisibleLayers();
-	    }
-	    activeDocument.trim(TrimType.TRANSPARENT,true,true,true,true);
-		if(activeDocument.width % 2 != 0) {
-        activeDocument.resizeCanvas(activeDocument.width + 1, activeDocument.height, AnchorPosition.MIDDLECENTER);
-		}
-		if(activeDocument.height % 2 != 0) {
-        activeDocument.resizeCanvas(activeDocument.width, activeDocument.height+ 1, AnchorPosition.MIDDLECENTER);
-		}
-	    var saveFile= File(path +"/"+outFolderName+"/"+lname+"@2x.png");
-		SavePNG(saveFile);		
-		activeDocument.resizeImage(undefined,undefined, app.activeDocument.resolution / 2, ResampleMethod.BICUBICSHARPER);
-		saveFile=File(path +"/"+outFolderName+"/"+lname+".png"); 
-		SavePNG(saveFile);
-	    app.activeDocument.close(SaveOptions.DONOTSAVECHANGES);
-	}
+        app.preferences.rulerUnits = Units.PIXELS
+        app.preferences.typeUnits = TypeUnits.PIXELS
+        app.displayDialogs = DialogModes.NO
+        
+        dupLayers();
+        if (shouldMerge === undefined || shouldMerge === true) {
+            activeDocument.mergeVisibleLayers();
+        }
+        activeDocument.trim(TrimType.TRANSPARENT, true, true, true, true);
+        if (activeDocument.width % 2 != 0) {
+//            alert('increase width from ' + activeDocument.width + " to " + (activeDocument.width + 1));
+            activeDocument.resizeCanvas(activeDocument.width + 1, activeDocument.height, AnchorPosition.MIDDLECENTER);
+        }
+        if (activeDocument.height % 2 != 0) {
+//            alert('increase height from ' + activeDocument.height + " to " + (activeDocument.height + 1));
+            activeDocument.resizeCanvas(activeDocument.width, activeDocument.height + 1, AnchorPosition.MIDDLECENTER);
+        }
+        var saveFile = File(lname + "@2x.png");
+        SavePNG(saveFile);
+        activeDocument.resizeImage(undefined, undefined, app.activeDocument.resolution / 2, ResampleMethod.BICUBICSHARPER);
+        saveFile = File(lname + ".png");
+        SavePNG(saveFile);
+        app.activeDocument.close(SaveOptions.DONOTSAVECHANGES);
+        
+        app.preferences.rulerUnits = startRulerUnits
+        app.preferences.typeUnits = startTypeUnits
+        app.displayDialogs = startDisplayDialogs
+    }
 }
 
 main();
 
-function dupLayers() { 
-        var descac = new ActionDescriptor();
-        var refac1 = new ActionReference();
-        refac1.putClass( charIDToTypeID('Dcmn') );
-	descac.putReference( charIDToTypeID('null'), refac1 );
-        descac.putString( charIDToTypeID('Nm  '), activeDocument.activeLayer.name );
-        var refac2 = new ActionReference();
-        refac2.putEnumerated( charIDToTypeID('Lyr '), charIDToTypeID('Ordn'), charIDToTypeID('Trgt') );
-        descac.putReference( charIDToTypeID('Usng'), refac2 );
-        executeAction( charIDToTypeID('Mk  '), descac, DialogModes.NO );
+function dupLayers() {
+    var descac = new ActionDescriptor();
+    var refac1 = new ActionReference();
+    refac1.putClass(charIDToTypeID('Dcmn'));
+    descac.putReference(charIDToTypeID('null'), refac1);
+    descac.putString(charIDToTypeID('Nm  '), activeDocument.activeLayer.name);
+    var refac2 = new ActionReference();
+    refac2.putEnumerated(charIDToTypeID('Lyr '), charIDToTypeID('Ordn'), charIDToTypeID('Trgt'));
+    descac.putReference(charIDToTypeID('Usng'), refac2);
+    executeAction(charIDToTypeID('Mk  '), descac, DialogModes.NO);
 }
 
-function SavePNG(saveFile){
-    var pngOpts = new ExportOptionsSaveForWeb; 
-    pngOpts.format = SaveDocumentType.PNG
-    pngOpts.PNG8 = false; 
-    pngOpts.transparency = true; 
-    pngOpts.interlaced = false; 
+function SavePNG(saveFile) {
+    var pngOpts = new ExportOptionsSaveForWeb;
+    pngOpts.format = SaveDocumentType.PNG;
+    pngOpts.PNG8 = false;
+    pngOpts.transparency = true;
+    pngOpts.interlaced = false;
     pngOpts.quality = 100;
-    activeDocument.exportDocument(new File(saveFile),ExportType.SAVEFORWEB,pngOpts); 
-	
+    activeDocument.exportDocument(new File(saveFile), ExportType.SAVEFORWEB, pngOpts);
+
 }
